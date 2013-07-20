154d09de4a83b82fa17cb90ba92555115d0c8e3e
Stefano Rivera <stefano@rivera.za.net>
2013-07-20 22:38:42 +0200

    Menu structure

--- settings.py
+++ settings.py
@@ -7,9 +7,26 @@ try:
 except ImportError:
     pass
 
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
 pyconzadir = os.path.dirname(__file__)
 
 
 STATICFILES_DIRS = (
     os.path.join(pyconzadir, 'static'),
-) + STATICFILES_DIRS
+)
+
+WAFER_MENUS += (
+    {"name": "sponsors", "label": _("Sponsors"),
+     "items": [
+         {"name": "sponsors", "label": _("Our sponsors"),
+          "url": reverse("wafer_sponsors")},
+         {"name": "packages", "label": _("Sponsorship packages"),
+          "url": reverse("wafer_sponsorship_packages")},
+     ]},
+    {"name": "talks", "label": _("Talks"),
+     "url": reverse("wafer_users_talks")},
+    {"name": "contact", "label": _("Contact"),
+     "url": reverse("wafer_page", args=('contact',))},
+)
